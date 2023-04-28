4631dfbedba7733936f9cee36386a03908d93cd7
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2023-04-28 17:17:40 -0500

    refactor edge split

--- src/edge_split.rs
+++ src/edge_split.rs
@@ -1,25 +1,26 @@
 use std::collections::{BTreeSet, HashMap, HashSet};
 
-/// Calculate new vertices and indices by splitting the edges in `edges`.
+/// Calculate new vertices and indices by splitting the edges in `edges_to_split`.
+/// The geometry must be triangulated!
+///
 /// This works similarly to Blender's "edge split" for calculating normals.
 // https://github.com/blender/blender/blob/a32dbb8/source/blender/geometry/intern/mesh_split_edges.cc
 pub fn split_edges<T: Copy>(
     vertices: &[T],
     vertex_indices: &[u32],
-    edges: &[[u32; 2]],
+    edges_to_split: &[[u32; 2]],
 ) -> (Vec<T>, Vec<u32>) {
     // TODO: should ldr_tools just store sharp edges?
-    // TODO: separate function and tests for this?
-    // Mark any vertices on a sharp edge as sharp to duplicate later.
-    let mut is_sharp_vertex = vec![false; vertices.len()];
-    let mut sharp_undirected_edges = HashSet::new();
-    for [v0, v1] in edges {
+    let mut should_split_vertex = vec![false; vertices.len()];
+    let mut undirected_edges = HashSet::new();
+    for [v0, v1] in edges_to_split {
         // Treat edges as undirected.
-        sharp_undirected_edges.insert([*v0, *v1]);
-        sharp_undirected_edges.insert([*v1, *v0]);
+        undirected_edges.insert([*v0, *v1]);
+        undirected_edges.insert([*v1, *v0]);
 
-        is_sharp_vertex[*v0 as usize] = true;
-        is_sharp_vertex[*v1 as usize] = true;
+        // Mark any vertices on an edge to split for duplication.
+        should_split_vertex[*v0 as usize] = true;
+        should_split_vertex[*v1 as usize] = true;
     }
 
     let old_adjacent_faces = adjacent_faces(vertices, vertex_indices);
@@ -28,16 +29,17 @@ pub fn split_edges<T: Copy>(
         vertices,
         vertex_indices,
         &old_adjacent_faces,
-        &is_sharp_vertex,
+        &should_split_vertex,
     );
 
+    // Keep track of the new vertex adjacency while merging edges.
     let mut new_adjacent_faces = adjacent_faces(&split_vertices, &split_vertex_indices);
 
     merge_duplicate_edges(
         &mut split_vertex_indices,
         vertex_indices,
         duplicate_edges,
-        sharp_undirected_edges,
+        undirected_edges,
         &old_adjacent_faces,
         &mut new_adjacent_faces,
     );
@@ -72,8 +74,7 @@ fn reindex_vertices<T: Copy>(
 }
 
 fn adjacent_faces<T>(vertices: &[T], vertex_indices: &[u32]) -> Vec<BTreeSet<usize>> {
-    // TODO: Function and tests for this since it's shared?
-    // TODO: Should this use the old non duplicated indices?
+    // TODO: Function and tests for this since it's shared with normals?
     // Assume the position indices are fully welded.
     // This simplifies calculating the adjacent face indices for each vertex.
     let mut adjacent_faces = vec![BTreeSet::new(); vertices.len()];
@@ -89,135 +90,137 @@ fn merge_duplicate_edges(
     split_vertex_indices: &mut [u32],
     vertex_indices: &[u32],
     duplicate_edges: HashSet<[u32; 2]>,
-    sharp_undirected_edges: HashSet<[u32; 2]>,
+    edges_to_split: HashSet<[u32; 2]>,
     old_adjacent_faces: &[BTreeSet<usize>],
     new_adjacent_faces: &mut [BTreeSet<usize>],
 ) {
-    // Merge any of the duplicated edges that is not marked sharp.
-    // We "merge" edges by ensuring they use the same vertex indices.
-    // TODO: Double check if there is redundant adjacency calculations with normals later.
-    // TODO: Just check duplicated edges here?
+    // The splitting step can create lots of duplicate vertices.
+    // Merge any of the duplicated edges that is not an edge to split.
     for [v0, v1] in duplicate_edges
         .into_iter()
-        .filter(|e| !sharp_undirected_edges.contains(e))
+        .filter(|e| !edges_to_split.contains(e))
     {
-        // Find the faces indicent to this edge from the old indexing.
+        // Find the faces indicent to this edge before splitting.
         let v0_faces = &old_adjacent_faces[v0 as usize];
         let v1_faces = &old_adjacent_faces[v1 as usize];
-        let mut faces = v0_faces.intersection(v1_faces);
+        let mut faces = v0_faces.intersection(v1_faces).copied();
 
         if let (Some(f0), Some(f1)) = (faces.next(), faces.next()) {
-            // "merge" this edge by using the F0 indices for the edge in F1
-            // The edges use the old indices that haven't been duplicated.
-            // This takes advantage of duplicate vertices not increasing the length of the face list.
-            // TODO: does this create redundant work?
-            // TODO: is it ok to always use the old non duplicated indices here?
-            let mut v0_f0 = v0;
-            let mut v1_f0 = v1;
-            // TODO: function to get vertex indices from face?
-            for i in f0 * 3..f0 * 3 + 3 {
-                // TODO: function and tests for this?
-                if vertex_indices[i] == v0 {
-                    v0_f0 = split_vertex_indices[i];
-                }
-                if vertex_indices[i] == v1 {
-                    v1_f0 = split_vertex_indices[i];
-                }
+            merge_verts_in_faces(
+                v0,
+                v1,
+                f0,
+                f1,
+                vertex_indices,
+                split_vertex_indices,
+                new_adjacent_faces,
+            );
+        }
+    }
+}
+
+fn merge_verts_in_faces(
+    v0: u32,
+    v1: u32,
+    f0: usize,
+    f1: usize,
+    vertex_indices: &[u32],
+    split_vertex_indices: &mut [u32],
+    new_adjacent_faces: &mut [BTreeSet<usize>],
+) {
+    // Merge an edge by merging both pairs of vertices.
+    // We can find the matching vertices using the old indexing.
+    // Merging each vertex pair also merges the adjacent faces.
+    let v0_f0 = find_old_vertex_in_face(v0, f0, vertex_indices, split_vertex_indices);
+    let v0_f1 = find_old_vertex_in_face(v0, f1, vertex_indices, split_vertex_indices);
+    new_adjacent_faces[v0_f0 as usize].extend(new_adjacent_faces[v0_f1 as usize].clone());
+
+    let v1_f0 = find_old_vertex_in_face(v1, f0, vertex_indices, split_vertex_indices);
+    let v1_f1 = find_old_vertex_in_face(v1, f1, vertex_indices, split_vertex_indices);
+    new_adjacent_faces[v1_f0 as usize].extend(new_adjacent_faces[v1_f1 as usize].clone());
+
+    // Update the verts in each of the adjacent faces to use the f0 verts.
+    // Use the new adjacency to keep track of what has already been merged.
+    let v0_faces = &new_adjacent_faces[v0_f0 as usize];
+    let v1_faces = &new_adjacent_faces[v1_f0 as usize];
+    for adjacent_face in v0_faces.iter().chain(v1_faces.iter()) {
+        for i in adjacent_face * 3..adjacent_face * 3 + 3 {
+            if vertex_indices[i] == v0 {
+                split_vertex_indices[i] = v0_f0;
             }
-
-            let mut v0_f1 = v0;
-            let mut v1_f1 = v1;
-            // TODO: function to get vertex indices from face?
-            for i in f1 * 3..f1 * 3 + 3 {
-                // TODO: function and tests for this?
-                if vertex_indices[i] == v0 {
-                    v0_f1 = split_vertex_indices[i];
-                }
-                if vertex_indices[i] == v1 {
-                    v1_f1 = split_vertex_indices[i];
-                }
-            }
-
-            // Merge an edge by merging both pairs of verts.
-            // The faces adjacent to the f1 verts are now adjacent to the f0 verts.
-            new_adjacent_faces[v0_f0 as usize].extend(new_adjacent_faces[v0_f1 as usize].clone());
-            new_adjacent_faces[v1_f0 as usize].extend(new_adjacent_faces[v1_f1 as usize].clone());
-
-            // Update the verts in each of the adjacent faces to use the f0 verts.
-            // Use the new adjacency to keep track of what has already been merged.
-            let v0_faces = &new_adjacent_faces[v0_f0 as usize];
-            let v1_faces = &new_adjacent_faces[v1_f0 as usize];
-            for adjacent_face in v0_faces.iter().chain(v1_faces.iter()) {
-                for i in adjacent_face * 3..adjacent_face * 3 + 3 {
-                    // TODO: match statement.
-                    // TODO: function and tests for this?
-                    if vertex_indices[i] == v0 {
-                        split_vertex_indices[i] = v0_f0;
-                    }
-                    if vertex_indices[i] == v1 {
-                        split_vertex_indices[i] = v1_f0;
-                    }
-                }
+            if vertex_indices[i] == v1 {
+                split_vertex_indices[i] = v1_f0;
             }
         }
     }
+}
+
+fn face_indices(vertex_indices: &[u32], face_index: usize) -> &[u32] {
+    &vertex_indices[face_index * 3..face_index * 3 + 3]
+}
+
+fn face_indices_mut(vertex_indices: &mut [u32], face_index: usize) -> &mut [u32] {
+    &mut vertex_indices[face_index * 3..face_index * 3 + 3]
+}
+
+fn find_old_vertex_in_face(
+    old_vertex_index: u32,
+    face_index: usize,
+    old_indices: &[u32],
+    new_indices: &[u32],
+) -> u32 {
+    // Find the corresponding vertex index in the new face.
+    face_indices(old_indices, face_index)
+        .iter()
+        .zip(face_indices(new_indices, face_index))
+        .find_map(|(old, new)| {
+            if *old == old_vertex_index {
+                Some(*new)
+            } else {
+                None
+            }
+        })
+        .unwrap()
 }
 
 fn split_face_verts<T: Copy>(
     vertices: &[T],
     vertex_indices: &[u32],
     adjacent_faces: &[BTreeSet<usize>],
-    is_sharp_vertex: &[bool],
+    should_split_vertex: &[bool],
 ) -> (Vec<T>, Vec<u32>, HashSet<[u32; 2]>) {
-    // Split sharp edges by duplicating the vertices.
+    // Split edges by duplicating the vertices.
     // This creates some duplicate edges to be cleaned up later.
     let mut split_vertices = vertices.to_vec();
     let mut split_vertex_indices = vertex_indices.to_vec();
 
     let mut duplicate_edges = HashSet::new();
 
-    // Iterate over all the indices of vertices marked as sharp.
-    for vertex_index in is_sharp_vertex
+    // Iterate over all the indices of marked vertices.
+    for vertex_index in should_split_vertex
         .iter()
         .enumerate()
-        .filter_map(|(v, sharp)| sharp.then_some(v))
+        .filter_map(|(v, split)| split.then_some(v))
     {
-        for (i_f, f) in adjacent_faces[vertex_index].iter().enumerate() {
-            let face = &mut split_vertex_indices[f * 3..f * 3 + 3];
+        for (i, f) in adjacent_faces[vertex_index].iter().enumerate() {
+            let face = face_indices_mut(&mut split_vertex_indices, *f);
 
-            // TODO: Find a cleaner way to calculate edges.
-            let mut i_face_vert = 0;
-            for (j, face_vert) in face.iter_mut().enumerate() {
-                if *face_vert == vertex_index as u32 {
-                    // Duplicate the vertex in all faces except the first.
-                    // The first face can just use the original index.
-                    if i_f > 0 {
+            // Duplicate the vertex in all faces except the first.
+            // The first face can just use the original index.
+            if i > 0 {
+                for face_vert in face.iter_mut() {
+                    if *face_vert == vertex_index as u32 {
                         *face_vert = split_vertices.len() as u32;
                         split_vertices.push(split_vertices[vertex_index]);
                     }
-
-                    i_face_vert = j;
                 }
             }
 
-            // Track any edges that have been duplicated.
-            // The non sharp duplicated edges will be merged later.
-            // Take advantage of every vertex being connected in a triangle.
-            // Use the original vertices sharp edges refer to the original indices.
-            let original_face = &vertex_indices[f * 3..f * 3 + 3];
-            let mut e0 = [
-                original_face[i_face_vert],
-                original_face[(i_face_vert + 1) % 3],
-            ];
-            // Edges are undirected, so normalize the direction for each edge.
-            e0.sort();
-            duplicate_edges.insert(e0);
+            // Find any edges that may need to be merged later.
+            let original_face = face_indices(vertex_indices, *f);
+            let (e0, e1) = find_incident_edges(original_face, vertex_index);
 
-            let mut e1 = [
-                original_face[i_face_vert],
-                original_face[(i_face_vert + 2) % 3],
-            ];
-            e1.sort();
+            duplicate_edges.insert(e0);
             duplicate_edges.insert(e1);
         }
     }
@@ -225,12 +228,26 @@ fn split_face_verts<T: Copy>(
     (split_vertices, split_vertex_indices, duplicate_edges)
 }
 
+fn find_incident_edges(face: &[u32], vertex_index: usize) -> ([u32; 2], [u32; 2]) {
+    // Take advantage of every vertex being connected in a triangle.
+    let i = face.iter().position(|v| *v == vertex_index as u32).unwrap();
+    let mut e0 = [face[i], face[(i + 1) % 3]];
+    let mut e1 = [face[i], face[(i + 2) % 3]];
+
+    // Edges are undirected, so normalize the direction for each edge.
+    // This avoids redundant merge operations later.
+    e0.sort();
+    e1.sort();
+
+    (e0, e1)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn split_sharp_edges_triangle_no_sharp_edges() {
+    fn split_edges_triangle_no_sharp_edges() {
         // 2
         // | \
         // 0 - 1
@@ -242,7 +259,7 @@ mod tests {
     }
 
     #[test]
-    fn split_sharp_edges_quad() {
+    fn split_edges_quad() {
         // Quad of two tris and one sharp edge.
         // The topology shouldn't change since 2-3 is already a boundary.
         // 2 - 3
@@ -257,7 +274,7 @@ mod tests {
     }
 
     #[test]
-    fn split_sharp_edges_two_quads() {
+    fn split_edges_two_quads() {
         // Two quads of two tris.
         // The topology shouldn't change.
         // 2 - 3 - 5
@@ -276,7 +293,7 @@ mod tests {
     }
 
     #[test]
-    fn split_sharp_edges_split_two_quads() {
+    fn split_edges_split_two_quads() {
         // Two quads of two tris and and one sharp edge.
         // 2 - 3 - 4
         // | \ | \ |
@@ -298,7 +315,7 @@ mod tests {
     }
 
     #[test]
-    fn split_sharp_edges_split_1_8cyli_dat() {
+    fn split_edges_split_1_8cyli_dat() {
         // Example taken from p/1-8cyli.dat.
         // 3 - 0 - 4
         // | / | / |
@@ -308,6 +325,7 @@ mod tests {
         // 3 - 2 - 5
         // | / | / |
         // 0 - 1 - 4
+
         assert_eq!(
             (
                 vec![2.0, 1.0, 0.0, 3.0, 5.0, 4.0],
